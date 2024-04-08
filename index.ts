7b729aa2a7f9844f04dfa91ed9a4f2e5b14b1a9b
Malaika Bilal <bmalaika88@gmail.com>
2024-04-08 13:52:58 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+
+let genNum = Math.floor(Math.random() * 2);
+
+let user = await inquirer.prompt({
+    name: 'numb1',
+    type:"list",
+    message:"please select the Number: ",
+    choices:['0','1'],
+});
+
+if (genNum == parseInt(user.numb1)) {
+    console.log(chalk.bold.blueBright("congratulations you won"));
+}
+else{
+    console.log(chalk.bold.redBright("Sorry, Better Luck Next Time"));
+}
+
